299b7a8dafa93e38fb2a521229164181a12f62f5
sadcoffeee <148781287+sadcoffeee@users.noreply.github.com>
2024-04-03 10:20:19 +0200

    fixed APIservice

--- app/src/main/java/com/example/miniprojektliste/network/FruitAPIService.kt
+++ app/src/main/java/com/example/miniprojektliste/network/FruitAPIService.kt
@@ -1,5 +1,4 @@
 package com.example.miniprojektliste.network
-import com.example.miniprojektliste.network
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
