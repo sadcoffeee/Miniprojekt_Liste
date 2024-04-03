89c431216a41efe320ee6de2db7f2b4f62878ebb
fridahelledie <151621828+fridahelledie@users.noreply.github.com>
2024-04-03 11:05:28 +0200

    Update HomeScreen.kt

--- app/src/main/java/com/example/miniprojektliste/screens/HomeScreen.kt
+++ app/src/main/java/com/example/miniprojektliste/screens/HomeScreen.kt
@@ -1,8 +1,5 @@
 package com.example.miniprojektliste.screens
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -20,7 +17,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,7 +25,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class HomeScreen {
-
 
     @Composable
     fun CardRow(items: List<String>, amount: List<String>) {
